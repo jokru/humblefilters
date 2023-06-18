7de59ee8625870503691cb60e6798bc5d9d2d9ab
jokru <ujoonas@hotmail.com>
2023-06-18 20:39:02 +0300

    Move name minification to utils.js

--- filters.js
+++ filters.js
@@ -14,31 +14,6 @@ function delay(time) {
 let ownedGames = []
 let wishlistGames = []
 
-let nameToAppID = {}
-let appIDToName = {}
-
-// The names might differ with non-alphanumeric characters and case
-
-const minifyRules = [
-    // Remove beginning numbers before dot, for store sidebar
-    /^\d+\. /g,
-    // Remove "the" from the beginning
-    /^the /g,
-    // Some games have "(steam)" in their name, but it's not part of the name
-    /(steam)/g,
-    // Remove non-alphanumeric characters
-    /[\W_]/g
-]
-
-const minifyName = (name) => {
-    if(!name) return
-    name = name.toLowerCase()
-    minifyRules.forEach(rule => {
-        name = name.replace(rule, '')
-    })
-    return name
-}
-
 // Time constants
 const _5min = 1000 * 60 * 5
 const _1hour = 1000 * 60 * 60
